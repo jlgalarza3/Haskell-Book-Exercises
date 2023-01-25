11f334e3912250d59b531f26a58830ba138ebeb0
jlgalarza3 <jorge2000cruz@gmail.com>
2023-01-25 10:12:14 -0500

    Complete haskell book exercises

--- Chapter2.hs
+++ Chapter2.hs
@@ -64,3 +64,34 @@ average ns = sum ns `div` length ns
 -> 2+(3*(4^5))
 
 -}
+
+-- 3. The script below contains three syntactic errors. Correct these errors and then check that your script works properly using GHCi.
+{-
+
+N = a 'div' length xs
+  where
+    a = 10
+    xs = [1,2,3,4,5]
+-}
+
+n = a `div` length xs
+  where
+    a = 10
+    xs = [1, 2, 3, 4, 5]
+
+{-
+4. The library function last selects the last element of a non-empty list; for example, last [1,2,3,4,5] = 5.
+ Show how the function last could be defined in terms of the other library functions introduced in this chapter.
+ Can you think of another possible definition?
+-}
+
+otherLast xs = head (reverse xs)
+
+{-
+5. The library function init removes the last element from a non-empty list; for example, init [1,2,3,4,5] = [1,2,3,4].
+ Show how init could similarly be defined in two different ways.
+-}
+
+otherInit1 xs = reverse (tail (reverse xs))
+
+otherInit2 xs = take (length xs - 1) xs
